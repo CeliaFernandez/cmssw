645d0a2b8dd77d93c3abd79f008582b62b811db2
CeliaFernandez <celia.fernandez.madrazo@cern.ch>
2021-12-04 17:04:46 +0100

    Add displacedMuon content that was left

--- RecoMuon/Configuration/python/RecoMuon_EventContent_cff.py
+++ RecoMuon/Configuration/python/RecoMuon_EventContent_cff.py
@@ -36,12 +36,16 @@ RecoMuonAOD = cms.PSet(
                                            #'drop *_muons_muons1stStep2muonsMap_*',
                                            #'drop recoIsoDepositedmValueMap_muons_*_*', #not really used
                                            #'drop doubleedmValueMap_muons_muPFIso*_*', #already inside the muon
-                                           # Tracks known by the Muon obj
+                                           # displacedMuons collection
                                            'keep recoMuons_displacedMuons_*_*',
+                                           'keep booledmValueMap_displacedMuons_*_*',
                                            'keep doubleedmValueMap_displacedMuons_dispMuPFMean*_*',
                                            'keep doubleedmValueMap_displacedMuons_dispMuPFSum*_*',
+                                           'keep recoMuonTimeExtraedmValueMap_displacedMuons_*_*',
+                                           'keep uintedmValueMap_displacedMuons_*_*',
                                            'keep *_particleFlow_displacedMuons_*',
                                            'keep recoIsoDepositedmValueMap_*Displaced_*_*',
+                                           # Tracks known by the Muon obj
                                            'keep recoTracks_standAloneMuons_*_*',
                                            'keep recoTrackExtras_standAloneMuons_*_*',
                                            'keep TrackingRecHitsOwned_standAloneMuons_*_*',
